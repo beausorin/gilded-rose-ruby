387ce58766782a7c0e7f66c850fdfbb092fbad42
Beau Sorin <beausorinwshs@gmail.com>
2024-02-28 03:15:51 -0600

    i literally tried everything i'm not sure how this code doesn't work i'm so lost

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,22 +1,12 @@
 module GildedRose
-  def self.new(name, quality, days_remaining)
-    klass_for(name).new(quality, days_remaining)
-  end
-
-  DEFAULT_CLASS = Item
-  SPECIALIZED_CLASSES = {
-    'normal'                                    => Normal,
-    'Aged Brie'                                 => Brie,
-    'Backstage passes to a TAFKAL80ETC concert' => Backstage}
-  def self.new(name, quality, days_remaining)
-    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).new(quality, days_remaining)
-  end
-
   class Item
     attr_reader :quality, :days_remaining
 
     def initialize(quality, days_remaining)
       @quality, @days_remaining = quality, days_remaining
+    end
+
+    def tick
     end
   end
 
@@ -26,7 +16,7 @@ module GildedRose
       return if @quality == 0
 
       @quality -= 1
-      @quality -= if @days_remaining <= 0
+      @quality -= 1 if @days_remaining <= 0
     end
   end
 
@@ -40,18 +30,37 @@ module GildedRose
     end
   end
 
-
   class Backstage < Item
     def tick
       @days_remaining -= 1
-      return              if @quality >= 50
+      return if @quality >= 50
       return @quality = 0 if @days_remaining < 0
 
       @quality += 1
-      @quality += 1 if @days_remaining < 10
       @quality += 1 if @days_remaining < 5
+      @quality += 1 if @days_remaining < 10
     end
   end
 
-end
+  class Conjured < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 2
+      @quality -= 2 if @days_remaining <= 0
+    end
+  end
+
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASSES = {
+    'Normal Item' => Normal,
+    'Aged Brie' => Brie,
+    'Backstage passes to a TAFKAL80ETC concert' => Backstage,
+    'Conjured Mana Cake' => Conjured
+  }
+
+  def self.new(name, quality, days_remaining)
+    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).new(quality, days_remaining)
+  end
 end
